81f3cebcf1b7107869e1595bc2bab7d43044bd38
CyrisXD <Cyris@cyris.io>
2017-11-09 20:29:27 +1300

    Update Port

--- index.js
+++ index.js
@@ -68,4 +68,4 @@ app.get('/webhook', (req, res) => {
 
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
