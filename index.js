3e35fb3bc71f0c97ac6754dfe560c7f6e70fbd3a
CyrisXD <Cyris@cyris.io>
2017-11-09 20:47:52 +1300

    Required Request Module

--- index.js
+++ index.js
@@ -4,7 +4,59 @@
 const
     express = require('express'),
     bodyParser = require('body-parser'),
+    request = require('request'),
     app = express().use(bodyParser.json()); // creates express http server
+
+
+
+
+
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {
+            "access_token": PAGE_ACCESS_TOKEN
+        },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+
+
 
 
 // Creates the endpoint for our webhook 
@@ -83,46 +135,8 @@ app.get('/webhook', (req, res) => {
 
 
 
-function handleMessage(sender_psid, received_message) {
-    
-        let response;
-    
-        // Check if the message contains text
-        if (received_message.text) {
-    
-            // Create the payload for a basic text message
-            response = {
-                "text": `You sent the message: "${received_message.text}". Now send me an image!`
-            }
-        }
-    
-        // Sends the response message
-        callSendAPI(sender_psid, response);
-    }
-    
-    function callSendAPI(sender_psid, response) {
-        // Construct the message body
-        let request_body = {
-          "recipient": {
-            "id": sender_psid
-          },
-          "message": response
-        }
-      
-        // Send the HTTP request to the Messenger Platform
-        request({
-          "uri": "https://graph.facebook.com/v2.6/me/messages",
-          "qs": { "access_token": PAGE_ACCESS_TOKEN },
-          "method": "POST",
-          "json": request_body
-        }, (err, res, body) => {
-          if (!err) {
-            console.log('message sent!')
-          } else {
-            console.error("Unable to send message:" + err);
-          }
-        }); 
-      }
+
+
 
 
 // Sets server port and logs message on success
