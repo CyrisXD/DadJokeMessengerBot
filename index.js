c5d3522cdeace70c72f38e9eba1e9cc65376191b
CyrisXD <Cyris@cyris.io>
2017-11-11 22:43:51 +1300

    Fixed bug on double webhooks

--- index.js
+++ index.js
@@ -107,14 +107,13 @@ app.post('/webhook', (req, res) => {
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                console.log(webhook_event.message);
-                if (webhook_event.message.nlp) {
+            if (webhook_event.message) {    
                     handleMessage(sender_psid, webhook_event.message);
-                } else if (webhook_event.postback) {
+            }
+              else if (webhook_event.postback) {
                     handlePostback(sender_psid, webhook_event.postback);
                 }
-            }
+            
 
         });
 
